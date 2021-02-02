daf52b071915c623ed1684e95aebc2f5a36ddbf9
Ashraf <nsh.aip@gmail.com>
2021-02-02 22:15:07 +0600

    Add medicine search filter

--- green_medic/apps/medicines/filters.py
+++ green_medic/apps/medicines/filters.py
@@ -1,11 +1,18 @@
+from django.db.models import Q
 from django_filters import FilterSet, CharFilter
 
 from green_medic.apps.medicines.models import Medicine
 
 
 class MedicineFilter(FilterSet):
+    def search_medicines(self, queryset, name, value):
+        return self.queryset.filter(
+            Q(brand_name=value) | Q(generic_name=value)
+        )
+
     brand_name = CharFilter(lookup_expr="icontains")
     generic_name = CharFilter(lookup_expr="icontains")
+    search = CharFilter(method='search_medicines')
 
     class Meta:
         model = Medicine
--- green_medic/apps/medicines/views.py
+++ green_medic/apps/medicines/views.py
@@ -2,6 +2,7 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import AllowAny
 
+from green_medic.apps.base.utils import get_visitor_ip_address
 from green_medic.apps.medicines.filters import MedicineFilter
 from green_medic.apps.medicines.models import Medicine
 from green_medic.apps.medicines.pagination import SetPagination15
@@ -17,8 +18,9 @@ class MedicineListView(ListAPIView):
     # permission_classes = [IsShopkeeperReadOnly | IsCustomerReadOnly]
     permission_classes = [AllowAny]
 
-    # def get(self, request, *args, **kwargs):
-    #     return super(MedicineListView, self).get(self, request, *args, **kwargs)
+    def get(self, request, *args, **kwargs):
+        print(get_visitor_ip_address(request=request))
+        return super(MedicineListView, self).get(self, request, *args, **kwargs)
 
 
 class MedicineRetrieveView(RetrieveAPIView):
